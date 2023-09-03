088852d09f64305ac5cb8fea49a9fcae9d7acc0e
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-09-03 11:22:58 +0200

    Lint

--- app/src/main/java/be/chvp/nanoledger/ui/add/AddActivity.kt
+++ app/src/main/java/be/chvp/nanoledger/ui/add/AddActivity.kt
@@ -24,8 +24,8 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
