c70d1dde4c5ce6f77b1bf5ce6a1df1d0f31b142b
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-12-17 10:20:16 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "be.chvp.nanoledger"
         minSdk = 26
         targetSdk = 33
-        versionCode = 10400
-        versionName = "0.1.3"
+        versionCode = 10500
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
