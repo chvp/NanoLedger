e725b0b706cf40af9a607162ec2da96b36e2b3ec
Charlotte Van Petegem <charlotte@vanpetegem.me>
2023-12-17 09:51:39 +0100

    Bump kotlin and compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
         viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.6"
     }
     packaging {
         resources {
