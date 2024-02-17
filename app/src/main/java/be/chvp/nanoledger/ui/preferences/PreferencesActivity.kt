9ca5ee581459395c4da7b385982cffa89f8b5bed
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-02-17 10:10:38 +0100

    Add FontWeight import

--- app/src/main/java/be/chvp/nanoledger/ui/preferences/PreferencesActivity.kt
+++ app/src/main/java/be/chvp/nanoledger/ui/preferences/PreferencesActivity.kt
@@ -40,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import be.chvp.nanoledger.R
 import be.chvp.nanoledger.ui.theme.NanoLedgerTheme
