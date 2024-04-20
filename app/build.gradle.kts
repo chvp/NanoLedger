765c1bfe7fcf733bbd79a76cf0ccd6b7583a35de
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2024-04-20 11:01:06 +0200

    Also bump compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.12"
     }
     packaging {
         resources {
