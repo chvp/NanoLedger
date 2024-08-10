4e51a0a9ceec25af5388b8230c5c72ae3318d9fd
Charlotte Van Petegem <charlotte@vanpetegem.be>
2024-08-10 14:15:17 +0200

    Update kotlin and compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
     packaging {
         resources {
