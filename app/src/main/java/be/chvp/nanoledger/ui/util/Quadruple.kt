fc88bf2404bffdf6fcddf90ad960bcc58f72268e
Antonio David Villegas Yeguas <antonio.villegas@murena.io>
2025-01-10 20:17:17 +0100

    Move the Posting class to it's own file

--- app/src/main/java/be/chvp/nanoledger/ui/util/Quadruple.kt
+++
@@ -1,8 +0,0 @@
-package be.chvp.nanoledger.ui.util
-
-import java.io.Serializable
-
-data class Quadruple<A, B, C, D>(var first: A, var second: B, var third: C, var fourth: D) :
-    Serializable {
-    override fun toString(): String = "($first, $second, $third, $fourth)"
-}
