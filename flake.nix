645b03245e88ef4b24712214b2b91fba7358eb1b
Charlotte Van Petegem <charlotte@vanpetegem.be>
2025-01-11 12:46:02 +0100

    Fix build tools version in flake

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; config = { android_sdk.accept_license = true; allowUnfree = true; }; overlays = [ devshell.overlays.default ]; };
-        buildToolsVersion = "34.0.0";
+        buildToolsVersion = "35.0.0";
         composed = pkgs.androidenv.composeAndroidPackages {
           buildToolsVersions = [ buildToolsVersion ];
           platformVersions = [ "35" ];
