12d53fe65d295d55654fee9351e59a5a8e6751cf
Charlotte Van Petegem <charlotte@vanpetegem.be>
2026-02-21 23:19:18 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "be.chvp.nanoledger"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2026022102
-        versionName = "1.4.0-beta.1"
+        versionCode = 2026022103
+        versionName = "1.4.0-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
